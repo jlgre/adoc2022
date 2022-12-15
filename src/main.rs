61c65f984cc2beb18cf12b1217a9b0d33651bcec
Luke Green <lukeg496@gmail.com>
2022-12-15 17:07:55 -0600

    Day 4 part 1

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ mod file;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     println!("Day 1: {:?}", day1::solve());
     println!("Day 2: {:?}", day2::solve());
     println!("Day 3: {:?}", day3::solve());
+    println!("Day 4: {}", day4::solve());
 }
